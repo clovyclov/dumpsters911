e1462bffffa543a757daa71817833144d5300f76
Clovis Shropshire <shropshireclovis@gmail.com>
2026-08-01 21:12:32 -0400

    Deploy live Sanity Studio editor to https://dumpsters911.sanity.studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,8 @@ export default defineCliConfig({
     projectId: 'u0hd089u',
     dataset: 'production',
   },
+  studioHost: 'dumpsters911',
+  deployment: {
+    appId: 'z06sl4hr8h3opj0i87mga6vu',
+  },
 });
